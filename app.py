906f138939b29ac623f088946b5068dfab41715d
van der Ham <jvdham@ubbink.nl>
2021-02-23 14:40:19 +0100

    Oefenen met git

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 app.layout = html.Div([
-    html.Label('Dropdown'),
+    html.Label('Indak / opdak'),
     dcc.Dropdown(
         id='dropdown_1',
         options=[
