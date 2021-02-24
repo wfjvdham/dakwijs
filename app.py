6d379aed983d26a8f80f63a8f45f4d28fdafdc74
Wim <wfjvdham@gmail.com>
2021-02-24 12:54:08 +0100

    Generate document

--- app.py
+++ app.py
@@ -7,6 +7,8 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
+from docx import Document
+from docx.shared import Inches
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
@@ -67,7 +69,10 @@ app.layout = html.Div([
         value=5,
     ),
     html.Br(),
+    html.Button('Download document', id='button'),
+    html.Br(),
     html.Div(id='output_1'),
+    html.P(id='placeholder'),
 ], style={'columnCount': 2})
 
 @app.callback(
@@ -77,6 +82,53 @@ app.layout = html.Div([
 )
 def update_output_div(input_value_1, input_value_2):
     return input_value_1 + ' en ' + ', '.join(input_value_2)
+
+@app.callback(
+    Output("placeholder", "children"),
+    Input('button', 'n_clicks')
+)
+def download(n_clicks):
+    document = Document()
+
+    document.add_heading('Document Title', 0)
+
+    p = document.add_paragraph('A plain paragraph having some ')
+    p.add_run('bold').bold = True
+    p.add_run(' and some ')
+    p.add_run('italic.').italic = True
+
+    document.add_heading('Heading, level 1', level=1)
+    document.add_paragraph('Intense quote', style='Intense Quote')
+
+    document.add_paragraph(
+        'first item in unordered list', style='List Bullet'
+    )
+    document.add_paragraph(
+        'first item in ordered list', style='List Number'
+    )
+
+    #document.add_picture('monty-truth.png', width=Inches(1.25))
+
+    records = (
+        (3, '101', 'Spam'),
+        (7, '422', 'Eggs'),
+        (4, '631', 'Spam, spam, eggs, and spam')
+    )
+
+    table = document.add_table(rows=1, cols=3)
+    hdr_cells = table.rows[0].cells
+    hdr_cells[0].text = 'Qty'
+    hdr_cells[1].text = 'Id'
+    hdr_cells[2].text = 'Desc'
+    for qty, id, desc in records:
+        row_cells = table.add_row().cells
+        row_cells[0].text = str(qty)
+        row_cells[1].text = id
+        row_cells[2].text = desc
+
+    document.add_page_break()
+    print('save document')
+    document.save('demo.docx')
 
 if __name__ == '__main__':
     app.run_server(host='0.0.0.0', port=8080, debug=True)
